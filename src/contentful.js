dceee0b1c65df791e2687a98785a9e7263d5c605
gbenimako@gmail.com <gbenimako@gmail.com>
2020-10-04 22:02:15 +0200

    refactoring

--- src/contentful.js
+++ src/contentful.js
@@ -1,5 +1,7 @@
 import { createClient } from "contentful";
+
 const { REACT_APP_API_SPACE, REACT_APP_ACCESS_TOKEN } = process.env;
+
 export default createClient({
   space: REACT_APP_API_SPACE,
   accessToken: REACT_APP_ACCESS_TOKEN,
